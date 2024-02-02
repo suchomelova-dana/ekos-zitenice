ec8d5185eeee5c04bcab211454ebfee33b395700
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-02-02 10:52:17 +0100

    dist folder added

---
+++ dist/data/Contacts.ts
@@ -0,0 +1,45 @@
+import {Contact} from "../../src/interfaces/Interfaces";
+
+const ContactArray : Contact[] = [
+    {
+        title: 'Obchodní jméno',
+        value: 'Ekos-Žitenice s.r.o.'
+    },
+    {
+        title: 'Adresa a sídlo firmy',
+        value: 'Litoměřická 17, 411 41 Žitenice',
+        icon: 'home'
+    },
+    {
+        title: 'Telefon',
+        value: '+420 777 057 313',
+        icon: 'phone'
+    },
+    {
+        title: 'E-mail',
+        value: 'office@ekos-zitenice.cz',
+        icon: 'envelope'
+    },
+    {
+        title: 'ID datové schránky',
+        value: '77h3e8p'
+    },
+    {
+        title: 'IČ',
+        value: '473 10 910'
+    },
+    {
+        title: 'DIČ',
+        value: 'CZ47310910'
+    },
+    {
+        title: 'Bankovní spoj',
+        value: 'ČSOB, a.s. - pobočka Litoměřiceč.ú. : 183138984/0300'
+    },
+    {
+        title: 'Zápis v OR',
+        value: 'dne 25. listopadu 1992 u Krajského soudu v Ústí nad Labem, oddíl C, vložka 3706'
+    },
+]
+
+export default ContactArray
