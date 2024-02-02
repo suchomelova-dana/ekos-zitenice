6cd3ea666e8624d43bddf1cba695e36f513802b3
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-02-02 13:55:41 +0100

    icons deleted

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
+// import { library } from '@fortawesome/fontawesome-svg-core'
 
 /* import font awesome icon component */
 // import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
