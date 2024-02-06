580bbb537cc66b782b6877bbd3a486523309829e
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-02-06 15:07:13 +0100

    build test

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,8 @@ cd dist
 git init
 git add -A
 git commit -m "new deplyment"
-git config user.email "danka.suchomelova@seznam.cz"
-git config user.name "suchomelova-dana"
-git push -f https://github.com/suchomelova-dana/ekos-zitenice.git master:gh-pages
+git remote add origin git@github.com:suchomelova-dana/ekos-zitenice.git
+git push -f origin master:gh-pages
 #
 cd -
 
