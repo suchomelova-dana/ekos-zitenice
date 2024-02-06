ab868e5f7a89f5d486c821b511c4d705f9ec3467
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-02-06 14:30:02 +0100

    build test

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #
-#set -e
+set -e
 #
-#yarn build
+yarn build
 #
-#cd dist
+cd dist
 #
-#git init
-#git add -A
-#git commit -m "new deplyment"
-#git push -f git@github.com:suchomelova-dana/ekos-zitenice.git master:gh-pages
+git init
+git add -A
+git commit -m "new deplyment"
+git push -f git@github.com:suchomelova-dana/ekos-zitenice.git master:gh-pages
 #
-#cd -
+cd -
 
