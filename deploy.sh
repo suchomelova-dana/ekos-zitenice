619564d9920c7f4679bcb82af057db97f4c52a20
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-02-06 14:59:20 +0100

    build test

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ cd dist
 git init
 git add -A
 git commit -m "new deplyment"
-git config --global user.email "danka.suchomelova@seznam.cz"
-git config --global user.name "suchomelova-dana"
+git config user.email "danka.suchomelova@seznam.cz"
+git config user.name "suchomelova-dana"
 git push -f https://github.com/suchomelova-dana/ekos-zitenice.git master:gh-pages
 #
 cd -
