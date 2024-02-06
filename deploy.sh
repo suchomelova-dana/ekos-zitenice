a5fcf36cdbba3f4cfc875d428feaaf05b52844ad
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-02-06 14:56:17 +0100

    build test

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ cd dist
 git init
 git add -A
 git commit -m "new deplyment"
-git push -f git@github.com:suchomelova-dana/ekos-zitenice.git master:gh-pages
+git push -f https://github.com/suchomelova-dana/ekos-zitenice.git master:gh-pages
 #
 cd -
 
