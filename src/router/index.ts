9ba7b1371f4093239d1f5b2fa25bec570915da73
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-02-10 14:36:48 +0100

    script src changes

--- src/router/index.ts
+++ src/router/index.ts
@@ -31,7 +31,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory('/ekos-zitenice/'),
+  history: createWebHashHistory(),
   scrollBehavior() {
     return { top: 0 }
   },
