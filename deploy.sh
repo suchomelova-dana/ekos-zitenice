b4a00fb2cafb511371c14ec12850dc77a283fd62
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-02-11 12:03:51 +0100

    script src changes

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-git remote add rigin git@github.com:suchomelova-dana/ekos-zitenice.git main:pages-test
+git remote add rigin git@github.com:suchomelova-dana/ekos-zitenice.git
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:suchomelova-dana/ekos-zitenice.git main:pages-test
