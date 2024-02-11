6380520235a982a2c3209aa718c3cfca7d7f3031
suchomelova-dana <danka.suchomelova@seznam.cz>
2024-02-11 17:22:39 +0100

    changes

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -4,12 +4,12 @@ const fs = require("fs");
 (async () => {
     try {
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
+        await execa("echo", ["ekos-zitenice.cz", ">","CNAME"]);
         console.log("Building...");
         await execa("npm", ["run", "build"]);
         // Understand if it's dist or build folder
         const folderName = fs.existsSync("dist") ? "dist" : "build";
-        await `echo 'ekos-zitenice.cz' > CNAME`
-        // await execa("echo", ['ekos-zitenice.cz', '>', 'CNAME']);
+        await execa("echo", ["ekos-zitenice.cz", ">","CNAME"]);
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
         await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
         console.log("Pushing to gh-pages...");
