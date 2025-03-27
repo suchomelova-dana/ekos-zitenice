d2592fff924b3626a92f83e7ec0355010fa0a24a
jan-svalich <163657141+jan-svalich@users.noreply.github.com>
2025-03-27 13:20:04 +0100

    Update ContactsPage.vue

--- src/views/ContactsPage.vue
+++ src/views/ContactsPage.vue
@@ -15,7 +15,7 @@
             <font-awesome-icon v-if="contact.icon" :icon="['fas', contact.icon]" />
           </div>
           <div class="title">{{contact.title}}:</div>
-          <div class="value">{{contact.value}}</div>
+          <a href="mailto:{{contact.email}}">{{contact.volue}}:</a>
         </div>
       </div>
     </div>
