107bb45c7242220ce81239004ceb970dcc1d0eee
jan-svalich <163657141+jan-svalich@users.noreply.github.com>
2025-04-01 15:02:51 +0200

    Update Kontakty.ts

--- public/data/Kontakty.ts
+++ public/data/Kontakty.ts
@@ -17,7 +17,7 @@ const ContactArray : Contact[] = [
     },
     {
         title: 'E-mail',
-        value:  <a href="mailto:office@ekos-zitenice.cz" class="mailAdress">office@ekos-zitenice.cz</a>'office@ekos-zitenice.cz',
+        value:  <a href="mailto:office@ekos-zitenice.cz" class="mailAdress">'office@ekos-zitenice.cz</a>',
         icon: 'envelope'
     },
     {
